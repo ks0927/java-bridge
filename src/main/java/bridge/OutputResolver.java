7f591f0564b2c879a2a1c7472558157b93b9143d
ks0927 <brian_als@naver.com>
2023-12-08 17:49:05 +0900

    feat: 게임 결과 출력 메서드 추가

--- src/main/java/bridge/OutputResolver.java
+++ src/main/java/bridge/OutputResolver.java
@@ -6,10 +6,10 @@ public class OutputResolver {
     public static final String START_PREFIX = "[";
     public static final String ROUND_SUFFIX = "|";
     public static final String END_SUFFIX = "]";
-
     public static final String SUCCESS_MARK = " O ";
     public static final String BLANK_MARK = "   ";
     public static final String FAIL_MARK = " X ";
+    private static final String GAME_RESULT_MESSAGE = "최종 게임 결과\n";
 
     public String resolveRoundResult(List<RoundResultDto> roundResultDtos) {
         String upPosition = START_PREFIX;
@@ -33,5 +33,20 @@ public class OutputResolver {
             return SUCCESS_MARK;
         }
         return FAIL_MARK;
+    }
+
+    public String resolveGameResult(GameResultDto gameResultDto) {
+        String result = GAME_RESULT_MESSAGE;
+        result += resolveRoundResult(gameResultDto.getGameResult());
+        result += "게임 성공 여부: " + makeResult(gameResultDto) + "\n";
+        result += "총 시도한 횟수: " + gameResultDto.getTotalCount() + "\n";
+        return result;
+    }
+
+    private String makeResult(GameResultDto gameResultDto) {
+        if (gameResultDto.isSuccess()) {
+            return "성공";
+        }
+        return "실패";
     }
 }
