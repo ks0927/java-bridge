87c1f028edc5a009952e7bdf38a016f5ab9ae51a
ks0927 <brian_als@naver.com>
2023-12-08 16:57:05 +0900

    feat: 게임 결과 Dto 추가

---
+++ src/main/java/bridge/GameResultDto.java
@@ -0,0 +1,27 @@
+package bridge;
+
+import java.util.List;
+
+public class GameResultDto {
+    private List<RoundResultDto> gameResult;
+    private boolean isSuccess;
+    private int totalCount;
+
+    public GameResultDto(List<RoundResultDto> gameResult, boolean isSuccess, int totalCount) {
+        this.gameResult = gameResult;
+        this.isSuccess = isSuccess;
+        this.totalCount = totalCount;
+    }
+
+    public List<RoundResultDto> getGameResult() {
+        return gameResult;
+    }
+
+    public boolean isSuccess() {
+        return isSuccess;
+    }
+
+    public int getTotalCount() {
+        return totalCount;
+    }
+}
