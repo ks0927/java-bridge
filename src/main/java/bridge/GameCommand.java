f1e77caf185d8c083d6ce303152cdd09d7af7192
ks0927 <brian_als@naver.com>
2023-12-08 16:38:36 +0900

    feat: 게임 재시작/종료 여부 enum 추가

---
+++ src/main/java/bridge/GameCommand.java
@@ -0,0 +1,24 @@
+package bridge;
+
+import java.util.Arrays;
+
+public enum GameCommand {
+    RESTART("R"),
+    QUIT("Q");
+
+    private static final String INVALID_COMMAND_MESSAGE = "[ERROR] 게임 재시도 및 종료는 R 과 Q만 가능합니다.";
+
+    private final String command;
+
+    GameCommand(String command) {
+        this.command = command;
+    }
+
+    public static GameCommand of(String target) {
+        return Arrays.stream(values())
+                .filter(gameCommand -> gameCommand.command.equals(target))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(INVALID_COMMAND_MESSAGE));
+    }
+
+}
