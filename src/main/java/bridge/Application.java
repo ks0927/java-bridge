2bdbaa50afd5b49b7c7d59bbb01aef53811642dd
ks0927 <brian_als@naver.com>
2023-12-08 18:44:59 +0900

    feat: config를 통한 실제 실행지점 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,5 +4,7 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BridgeConfig bridgeConfig = new BridgeConfig();
+        bridgeConfig.bridgeController().run();
     }
 }
